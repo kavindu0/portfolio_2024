005cde82b9030bb09025f0973fd0c9a432bff6ed
kavi castelo <kavindu123450@gmail.com>
2023-12-03 22:57:50 +0530

    first note part 2

--- src/app/components/js/v8engine/v8engine-cont/v8engine-cont.component.ts
+++ src/app/components/js/v8engine/v8engine-cont/v8engine-cont.component.ts
@@ -1,4 +1,5 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, HostListener, OnInit, ViewEncapsulation} from '@angular/core';
+import {ScrollService} from "../../../../services/scroll.service";
 
 @Component({
   selector: 'app-v8engine-cont',
@@ -6,6 +7,33 @@ import {Component, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./v8engine-cont.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class V8engineContComponent {
+export class V8engineContComponent implements OnInit{
 
+  sections = [
+    'Unveiling the Intricacies..',
+    'Introduction:',
+    '1. The V8 Configuration:',
+    '2. Power and Performance..',
+    '3. Smooth Operation and..',
+    '4. Torque Across the..',
+    '5. Versatility Across..',
+    '6. The Signature Sound:',
+    '7. Integration of Advanced..',
+    '8. Customization and..',
+    'Conclusion:',
+  ];
+
+  constructor(private scrollService: ScrollService) { }
+
+  ngOnInit() {
+    this.scrollService.section$.subscribe(section => {
+      // Do something with the active section (e.g., highlight it)
+    });
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event:any) {
+    // Determine the active section based on scroll position
+    // Notify the ScrollService about the active section
+  }
 }
--- src/app/components/js/v8engine/v8engine.component.ts
+++ src/app/components/js/v8engine/v8engine.component.ts
@@ -1,4 +1,5 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {ScrollService} from "../../../services/scroll.service";
 
 @Component({
   selector: 'app-v8engine',
@@ -6,6 +7,33 @@ import {Component, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./v8engine.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class V8engineComponent {
+export class V8engineComponent implements OnInit {
 
+  sections = [
+    'Unveiling the Intricacies..',
+    'Introduction:',
+    '1. The V8 Configuration:',
+    '2. Power and Performance..',
+    '3. Smooth Operation and..',
+    '4. Torque Across the..',
+    '5. Versatility Across..',
+    '6. The Signature Sound:',
+    '7. Integration of Advanced..',
+    '8. Customization and..',
+    'Conclusion:',
+  ];
+  activeSection: string | any;
+
+  constructor(private scrollService: ScrollService) {
+  }
+
+  ngOnInit(): void {
+    this.scrollService.section$.subscribe(section => {
+      this.activeSection = section;
+    });
+  }
+
+  onNavClick(section: string) {
+    this.scrollService.scrollToSection(section);
+  }
 }

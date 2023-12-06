7fc9b695d07af68c893d2f79337b6a779ccf1f9c
kavi castelo <kavindu123450@gmail.com>
2023-12-06 20:16:57 +0530

    third note init section 4

--- src/app/components/js/wave-simulation/wave-simulation-cont/wave-simulation-cont.component.ts
+++ src/app/components/js/wave-simulation/wave-simulation-cont/wave-simulation-cont.component.ts
@@ -1,12 +1,22 @@
-import {Component, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+  ViewChild,
+  ViewEncapsulation
+} from '@angular/core';
 import {ScrollService} from "../../../../services/scroll.service";
 
 @Component({
   selector: 'app-wave-simulation-cont',
   templateUrl: './wave-simulation-cont.component.html',
-  styleUrls: ['./wave-simulation-cont.component.scss']
+  styleUrls: ['./wave-simulation-cont.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class WaveSimulationContComponent implements OnInit {
+export class WaveSimulationContComponent implements OnInit, AfterViewInit {
 
   sections = [
     'Simulating Damped Waves with..',
@@ -17,8 +27,30 @@ export class WaveSimulationContComponent implements OnInit {
     '4. JavaScript Implementation:',
     'Conclusion:',
   ];
+  gistUrl: string = 'https://gist.github.com/kavicastelo/a48e49be7e8fd5b166648415312189af';
 
-  constructor(private scrollService: ScrollService, private renderer: Renderer2, private el: ElementRef) { }
+  @ViewChild('iframe', {static: false}) iframe: any;
+
+  ngAfterViewInit() {
+    const doc = this.iframe.nativeElement.contentDocument || this.iframe.nativeElement.contentElement.contentWindow;
+    const content = `
+    <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <base target="_parent"><title>bjkkj</title>
+        </head>
+        <body onload="parent.document.getElementById('${this.iframe.nativeElement.id}')">
+        <script type="text/javascript" src="${this.gistUrl}.js"></script>
+        </body>
+      </html>
+    `;
+    doc.open();
+    doc.write(content);
+    doc.close();
+  }
+
+  constructor(private scrollService: ScrollService, private renderer: Renderer2, private el: ElementRef) {
+  }
 
   ngOnInit() {
     this.scrollService.section$.subscribe(section => {
@@ -32,7 +64,7 @@ export class WaveSimulationContComponent implements OnInit {
   }
 
   @HostListener('window:scroll', ['$event'])
-  onScroll(event:any) {
+  onScroll(event: any) {
     // Determine the active section based on scroll position
     // Notify the ScrollService about the active section
   }

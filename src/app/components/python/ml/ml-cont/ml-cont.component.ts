9f1ef047cd4d9216903d17b2b14c7022c51bd05d
kavi castelo <kavindu123450@gmail.com>
2023-12-07 20:24:08 +0530

    6th note section 1

--- src/app/components/python/ml/ml-cont/ml-cont.component.ts
+++ src/app/components/python/ml/ml-cont/ml-cont.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, HostListener, Renderer2} from '@angular/core';
+import {ScrollService} from "../../../../services/scroll.service";
 
 @Component({
   selector: 'app-ml-cont',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class MlContComponent {
 
+  sections = [
+    'The Comprehensive..',
+    'Introduction:',
+    '1. Unveiling the Basics:',
+    '‎ ‎ ➥ Unraveling the Essence..',
+    '‎ ‎ ➥ Navigating Types of..',
+    '2. Key Concepts Illuminated:',
+    '‎ ‎ ➥ Pioneering Feature..',
+    '‎ ‎ ➥ Charting Model..',
+    '‎ ‎ ➥ Navigating Overfitting..',
+    '3. A Sojourn into Advanced..',
+    '‎ ‎ ➥ The Depths of Deep..',
+    '‎ ‎ ➥ Harnessing Transfer..',
+    '‎ ‎ ➥ The Advancements in..',
+    '4. The Practical Frontier:',
+    '‎ ‎ ➥ Selecting the Right..',
+    '‎ ‎ ➥ Crafting Real-world..',
+    '‎ ‎ ➥ Sustaining a State of..',
+    'Conclusion:',
+  ];
+
+  constructor(private scrollService: ScrollService, private renderer: Renderer2, private el: ElementRef) { }
+
+  ngOnInit() {
+    this.scrollService.section$.subscribe(section => {
+      // Remove highlight from all sections
+      // this.sections.forEach(s => this.renderer.removeClass(this.el.nativeElement.querySelector(`#${s}`), 'highlight'));
+
+      // Add highlight to the active section
+      // this.renderer.addClass(this.el.nativeElement.querySelector(`#${section}`), 'highlight');
+
+    });
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event:any) {
+    // Determine the active section based on scroll position
+    // Notify the ScrollService about the active section
+  }
 }
--- src/app/components/python/ml/ml.component.ts
+++ src/app/components/python/ml/ml.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ScrollService} from "../../../services/scroll.service";
 
 @Component({
   selector: 'app-ml',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class MlComponent {
 
+  sections = [
+    'The Comprehensive..',
+    'Introduction:',
+    '1. Unveiling the Basics:',
+    '‎ ‎ ➥ Unraveling the Essence..',
+    '‎ ‎ ➥ Navigating Types of..',
+    '2. Key Concepts Illuminated:',
+    '‎ ‎ ➥ Pioneering Feature..',
+    '‎ ‎ ➥ Charting Model..',
+    '‎ ‎ ➥ Navigating Overfitting..',
+    '3. A Sojourn into Advanced..',
+    '‎ ‎ ➥ The Depths of Deep..',
+    '‎ ‎ ➥ Harnessing Transfer..',
+    '‎ ‎ ➥ The Advancements in..',
+    '4. The Practical Frontier:',
+    '‎ ‎ ➥ Selecting the Right..',
+    '‎ ‎ ➥ Crafting Real-world..',
+    '‎ ‎ ➥ Sustaining a State of..',
+    'Conclusion:',
+  ];
+  activeSection: string | any;
+
+  constructor(private scrollService: ScrollService) {
+  }
+
+  ngOnInit(): void {
+    this.scrollService.section$.subscribe(section => {
+      this.activeSection = section;
+    });
+  }
+
+  onNavClick(section: string) {
+    this.scrollService.scrollToSection(section);
+  }
 }

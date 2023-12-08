c1715194e284d402812d8762e576abd679554491
kavi castelo <kavindu123450@gmail.com>
2023-12-08 10:59:19 +0530

    create different table of content sections according to router link

--- src/app/components/python/ml/ml.component.ts
+++ src/app/components/python/ml/ml.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {ScrollService} from "../../../services/scroll.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-ml',
@@ -8,35 +9,43 @@ import {ScrollService} from "../../../services/scroll.service";
 })
 export class MlComponent {
 
-  sections = [
-    'The Comprehensive..',
-    'Introduction:',
-    '1. Unveiling the Basics:',
-    '‎ ‎ ➥ Unraveling the Essence..',
-    '‎ ‎ ➥ Navigating Types of..',
-    '2. Key Concepts Illuminated:',
-    '‎ ‎ ➥ Pioneering Feature..',
-    '‎ ‎ ➥ Charting Model..',
-    '‎ ‎ ➥ Navigating Overfitting..',
-    '3. A Sojourn into Advanced..',
-    '‎ ‎ ➥ The Depths of Deep..',
-    '‎ ‎ ➥ Harnessing Transfer..',
-    '‎ ‎ ➥ The Advancements in..',
-    '4. The Practical Frontier:',
-    '‎ ‎ ➥ Selecting the Right..',
-    '‎ ‎ ➥ Crafting Real-world..',
-    '‎ ‎ ➥ Sustaining a State of..',
-    'Conclusion:',
-  ];
+  sections:any = [];
   activeSection: string | any;
 
-  constructor(private scrollService: ScrollService) {
+  constructor(private scrollService: ScrollService, private router: Router) {
   }
 
   ngOnInit(): void {
     this.scrollService.section$.subscribe(section => {
       this.activeSection = section;
     });
+
+    if (this.router.url === '/python/machine-learning') {
+      this.sections = [
+        'The Comprehensive..',
+        'Introduction:',
+        '1. Unveiling the Basics:',
+        '‎ ‎ ➥ Unraveling the Essence..',
+        '‎ ‎ ➥ Navigating Types of..',
+        '2. Key Concepts Illuminated:',
+        '‎ ‎ ➥ Pioneering Feature..',
+        '‎ ‎ ➥ Charting Model..',
+        '‎ ‎ ➥ Navigating Overfitting..',
+        '3. A Sojourn into Advanced..',
+        '‎ ‎ ➥ The Depths of Deep..',
+        '‎ ‎ ➥ Harnessing Transfer..',
+        '‎ ‎ ➥ The Advancements in..',
+        '4. The Practical Frontier:',
+        '‎ ‎ ➥ Selecting the Right..',
+        '‎ ‎ ➥ Crafting Real-world..',
+        '‎ ‎ ➥ Sustaining a State of..',
+        'Conclusion:',
+      ];
+      this.scrollService.scrollToSection('The Comprehensive..');
+    }
+    if (this.router.url === '/python/geographic-analysis/') {
+      this.scrollService.scrollToSection('Introduction:');
+    }
   }
 
   onNavClick(section: string) {

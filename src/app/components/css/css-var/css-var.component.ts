c070e85ce6c7c99835de324e65d1f1148d3d85ed
kavi castelo <kavindu123450@gmail.com>
2023-12-11 18:05:50 +0530

    note 9 init base

--- src/app/components/css/css-var/css-var.component.ts
+++ src/app/components/css/css-var/css-var.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ScrollService} from "../../../services/scroll.service";
 
 @Component({
   selector: 'app-css-var',
   templateUrl: './css-var.component.html',
   styleUrls: ['./css-var.component.scss']
 })
-export class CssVarComponent {
+export class CssVarComponent implements OnInit{
 
+  sections = [
+    'CSS3 vs. SCSS: Navigating..',
+    'Introduction:',
+    '1. CSS3: A Glimpse of..',
+    '2. SCSS: Elevating the..',
+    '3. Advantages of SCSS..',
+    '‎ ‎ ➥ Variables:',
+    '‎ ‎ ➥ Nesting:',
+    '‎ ‎ ➥ Mixins:',
+    '‎ ‎ ➥ Partials and Import:',
+    '‎ ‎ ➥ Math Operations:',
+    '4. Disadvantages of SCSS:',
+    '‎ ‎ ➥ Learning Curve:',
+    '‎ ‎ ➥ Compilation Step:',
+    '5. SCSS in Action: Real..',
+    '‎ ‎ ➥ Google Maps:',
+    '‎ ‎ ➥ SpaceX Starlink:',
+    'Conclusion:',
+  ];
+  activeSection: string | any;
+
+  constructor(private scrollService: ScrollService) {
+  }
+
+  ngOnInit(): void {
+    this.scrollService.section$.subscribe(section => {
+      this.activeSection = section;
+    });
+  }
+
+  onNavClick(section: string) {
+    this.scrollService.scrollToSection(section);
+  }
 }
--- src/app/components/css/css-vs-scss/css-vs-scss.component.ts
+++ src/app/components/css/css-vs-scss/css-vs-scss.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ScrollService} from "../../../services/scroll.service";
 
 @Component({
@@ -6,7 +6,7 @@ import {ScrollService} from "../../../services/scroll.service";
   templateUrl: './css-vs-scss.component.html',
   styleUrls: ['./css-vs-scss.component.scss']
 })
-export class CssVsScssComponent {
+export class CssVsScssComponent implements OnInit{
 
   sections = [
     'CSS3 vs. SCSS: Navigating..',

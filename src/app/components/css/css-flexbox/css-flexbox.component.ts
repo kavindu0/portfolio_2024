f2cff6d8a9c274ed1fc8a4e14eba819fab9c9d65
kavi castelo <kavindu123450@gmail.com>
2024-02-07 16:48:13 +0530

    flexbox component content

--- src/app/components/css/css-flexbox/css-flexbox.component.ts
+++ src/app/components/css/css-flexbox/css-flexbox.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ScrollService} from "../../../services/scroll.service";
 
 @Component({
   selector: 'app-css-flexbox',
   templateUrl: './css-flexbox.component.html',
   styleUrls: ['./css-flexbox.component.scss']
 })
-export class CssFlexboxComponent {
+export class CssFlexboxComponent implements OnInit {
+
+  sections = [
+    'Mastering CSS Flexbox:',
+    'Understanding Flexbox',
+    '‎ ‎ ➥ What is Flexbox?',
+    '‎ ‎ ➥ Key Concepts',
+    'Working with Flex Container',
+    '‎ ‎ ➥ Properties for..',
+    'Styling Flex Items',
+    '‎ ‎ ➥ Properties for..',
+    'Responsive Design with Flexbox',
+    '‎ ‎ ➥ Example: Responsive..',
+    'Conclusion:',
+  ];
+  activeSection: string | any;
+
+  constructor(private scrollService: ScrollService) {
+  }
+
+  ngOnInit(): void {
+    this.scrollService.section$.subscribe(section => {
+      this.activeSection = section;
+    });
+  }
+
+  onNavClick(section: string) {
+    this.scrollService.scrollToSection(section);
+  }
 
 }

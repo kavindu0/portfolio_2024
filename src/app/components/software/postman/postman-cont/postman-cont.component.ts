cdd6fa769112e3d15f96ab37d18348e3e0313320
kavi castelo <kavindu123450@gmail.com>
2024-02-09 17:33:56 +0530

    init postman content component
    Co-Authored-By: kavindu0 <142529122+kavindu0@users.noreply.github.com>

---
+++ src/app/components/software/postman/postman-cont/postman-cont.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-postman-cont',
+  templateUrl: './postman-cont.component.html',
+  styleUrls: ['./postman-cont.component.scss']
+})
+export class PostmanContComponent {
+
+}

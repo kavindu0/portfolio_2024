30cbeb001b06ba6ee5612feb8e519d94a3d9c81f
kavi castelo <kavindu123450@gmail.com>
2024-02-09 18:02:50 +0530

    add dummy content to the postman component

--- src/app/components/software/postman/postman.component.ts
+++ src/app/components/software/postman/postman.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ScrollService} from "../../../services/scroll.service";
 
 @Component({
   selector: 'app-postman',
   templateUrl: './postman.component.html',
   styleUrls: ['./postman.component.scss']
 })
-export class PostmanComponent {
+export class PostmanComponent implements OnInit {
+  sections = [
+    'Unveiling Postman',
+    'Introduction',
+    '1. Getting Started with Postman',
+    '2. Conducting Analyses in Postman',
+    '3. Advantages of Using Postman',
+    'Conclusion',
+  ]
+  activeSection: string | any
+  constructor(private scrollService: ScrollService) {
+  }
+  ngOnInit(): void {
+    this.scrollService.section$.subscribe(section => {
+      this.activeSection = section;
+    });
+  }
+
+  onNavClick(section: string) {
+    this.scrollService.scrollToSection(section);
+  }
 
 }
